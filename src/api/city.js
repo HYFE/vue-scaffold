02c89bd5fdb7b7eb4819cb8f5d2fcedf0130ecd0
yscoder <634206017@qq.com>
2017-03-22 11:17:07 +0800

    remove dot

--- src/api/city.js
+++ src/api/city.js
@@ -1,7 +1,7 @@
 import Ajax, { GET } from './ajax'
 
 // 通过 Ajax 类实例化拥有基础的 QRUD 能力
-const CityApi = new Ajax('./city')
+const CityApi = new Ajax('/city')
 
 // 如果需要额外的个性化请求，借助 GET、POST、PUT、DELETE 工具方法添加
 CityApi.paging = GET('/city')
