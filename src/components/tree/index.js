3fa9ba9eb653c7b04bfc66801497d857c95425d6
yscoder <634206017@qq.com>
2017-03-25 11:42:15 +0800

    误操作

---
+++ src/components/tree/index.js
@@ -0,0 +1,5 @@
+import tree from './tree.vue'
+import treeItem from './treeItem.vue'
+
+export default tree
+export { treeItem }
---
+++ src/components/tree/tree.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <template
+            v-if="data instanceof Array"
+            v-for="item in data">
+            <tree-item :data="item"></tree-item>
+        </template>
+        <tree-item v-else :data="data"></tree-item>
+    </div>
+</template>
+<script>
+import treeItem from './treeItem'
+
+export default {
+    components: {
+        treeItem
+    },
+    props: {
+        data: [Object, Array]
+    },
+    methods: {
+
+    }
+}
+</script>
