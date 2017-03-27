6c019594ff0c0c0e0a0bbd5b3e25ff1de055bcf4
yscoder <634206017@qq.com>
2017-03-27 11:21:08 +0800

    localStoage fallback

--- src/util/cache.js
+++ src/util/cache.js
@@ -1,7 +1,48 @@
-const get = key => JSON.parse(localStorage.getItem(key))
-const save = (key, data) => localStorage.setItem(key, JSON.stringify(data))
+/**
+ * localStorage
+ */
+const __localStorage = window.localStorage
 
-export default {
-    get,
-    save
+/**
+ * 检测是否支持 localStorage
+ */
+const isSupported = () => {
+    const key = '__EPM_TEST_SUPPORTED__'
+    try {
+        __localStorage.setItem(key, Date.now())
+        __localStorage.removeItem(key)
+        return true
+    } catch (error) {
+        return false
+    }
 }
+
+/**
+ * 封装 browserStorage
+ */
+const browserStorage = () => {
+    const storage = __localStorage
+    return {
+        get: key => JSON.parse(storage.getItem(key)),
+        save: (key, data) => storage.setItem(key, JSON.stringify(data))
+    }
+}
+
+/**
+ * ramStorage
+ */
+const ramStorage = () => {
+    const storage = {}
+    return {
+        get: key => storage[key],
+        save: (key, data) => {
+            storage[key] = data
+        },
+    }
+}
+
+/**
+ * 不支持 localStorage 时，回退 ramStorage
+ */
+const cacheStore = isSupported() ? browserStorage() : ramStorage()
+export default cacheStore
