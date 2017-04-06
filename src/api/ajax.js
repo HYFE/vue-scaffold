4c2983bc91cf7eb88b3b140365976a7bf7874d0b
yscoder <634206017@qq.com>
2017-04-06 18:11:02 +0800

    ajax 功能增强

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { ROOT_PATH } from './config'
 
 const FORM_CONTENT_TYPE = 'application/x-www-form-urlencoded'
 axios.defaults.headers.post['Content-Type'] = FORM_CONTENT_TYPE
@@ -6,34 +7,66 @@ axios.defaults.headers.put['Content-Type'] = FORM_CONTENT_TYPE
 
 const Ajax = axios.create({
     // 公共 HTTP 配置
-    baseURL: '/api',
+    baseURL: ROOT_PATH,
     // timeout: 1000,
     // headers: { 'X-Custom-Header': 'foobar' },
     responseType: 'json'
 })
 
+/**
+ * 格式化参数
+ * @param {*} params Object
+ */
 const QS = (params = {}) => {
     let url = ''
     Object.keys(params).forEach((k, i) => {
-        url = url + `${i === 0 ? '' : '&'}${k}=${params[k]}`
+        url += `${i === 0 ? '' : '&'}${k}=${params[k]}`
     })
     return url
 }
 
 const HTTP_HANDLE = req => req.then(res => res.data).catch(err => console.error('[API]:', err))
 
-// 工具函数
+/**
+ * http GET
+ * @param {*} url String
+ * @param {*} params Object
+ */
 export const GET = (url, params) => HTTP_HANDLE(Ajax.get(url, params ? {
-    params
-} : params))
-export const POST = (url, body) => HTTP_HANDLE(Ajax.post(url, QS(body)))
-export const PUT = (url, body) => HTTP_HANDLE(Ajax.put(url, QS(body)))
-export const DELETE = (url, params) => HTTP_HANDLE(Ajax.delete(url, params ? {
     params
 } : params))
 
 /**
- * 对常用 Rest 请求格式的封装
+ * http POST
+ * @param {*} url String
+ * @param {*} body Object
+ */
+export const POST = (url, body) => HTTP_HANDLE(Ajax.post(url, QS(body)))
+
+/**
+ * http PUT
+ * @param {*} url String
+ * @param {*} body Object
+ */
+export const PUT = (url, body) => HTTP_HANDLE(Ajax.put(url, QS(body)))
+
+/**
+ * http DELETE
+ * @param {*} url String
+ * @param {*} params Object
+ */
+export const DELETE = (url, params) => HTTP_HANDLE(Ajax.delete(url, params ? {
+    params
+} : params))
+
+const fixUrl = url => (url.lastIndexOf('/') === url.length ? url : `${url}/`)
+const urlPattern = (url, arg) => ({
+    url: url.replace(/:[a-zA-Z]+/g, m => arg.shift()),
+    params: arg.length ? arg[0] : null
+})
+
+/**
+ * 对常用 RESTful 请求格式的封装，支持多级资源请求
  * 以一个实体 User 为例，基础 URL 为 `/api/user`，则：
  * 查询 GET /api/user/:id
  * 添加 POST /api/user
@@ -45,32 +78,31 @@ export const DELETE = (url, params) => HTTP_HANDLE(Ajax.delete(url, params ? {
  */
 export default class Api {
     constructor(url) {
-        this.url = this.fixUrl(url)
+        this.url = fixUrl(url)
     }
 
-    fixUrl(url) {
-        return url.lastIndexOf('/') === url.length ? url : url + '/'
+    add(...args) {
+        const { url, params } = urlPattern(this.url, args)
+        return POST(url, params)
     }
 
-    add(m) {
-        return POST(this.url, m)
+    update(...args) {
+        const { url, params } = urlPattern(`${this.url}:id`, args)
+        return PUT(url, params)
     }
 
-    update(id, m) {
-        return PUT(this.url + id, m)
+    delete(...args) {
+        const { url, params } = urlPattern(`${this.url}:id`, args)
+        return DELETE(url, params)
     }
 
-    delete(id, params) {
-        return DELETE(this.url + id, params ? {
-            params
-        } : params)
+    one(...args) {
+        const { url, params } = urlPattern(`${this.url}:id`, args)
+        return GET(url, params)
     }
 
-    one(id, params) {
-        return GET(this.url + id, params)
-    }
-
-    all(params) {
-        return GET(this.url, params)
+    all(...args) {
+        const { url, params } = urlPattern(this.url, args)
+        return GET(url, params)
     }
 }
